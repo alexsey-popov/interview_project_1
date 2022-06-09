ccdcefc730864b03067a0cb68d6c81f91cca0817
Alexsey <send.popov@mail.ru>
2022-06-09 16:45:43 +0300

    Init. Create Equipment

---
+++ database/migrations/2022_06_09_105319_create_equipment_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('equipment_type_id')->constrained()->onDelete('cascade');
+            $table->string('serial_number')->unique();
+            $table->string('notes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipment');
+    }
+}
