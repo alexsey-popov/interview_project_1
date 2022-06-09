95eb461142e91c65ebfa3d9575a17075b8fc4151
Alexsey at home <send.popov@mail.ru>
2022-06-09 21:11:26 +0300

    Refactor. EquipmentTypeSeeder

--- database/seeders/EquipmentTypeSeeder.php
+++ database/seeders/EquipmentTypeSeeder.php
@@ -15,25 +15,18 @@ class EquipmentTypeSeeder extends Seeder
      */
     public function run()
     {
-        $date = Carbon::now();
-        EquipmentType::insert([
+        EquipmentType::factory()->createMany([
             [
                 'name' => 'TP-Link TL-WR74',
                 'mask' => 'XXAAAAAXAA',
-                'created_at' => $date,
-                'updated_at' => $date,
             ],
             [
                 'name' => 'D-Link DIR-300',
                 'mask' => 'NXXAAXZXaa',
-                'created_at' => $date,
-                'updated_at' => $date,
             ],
             [
                 'name' => 'D-Link DIR-300 S',
                 'mask' => 'NXXAAXZXXX',
-                'created_at' => $date,
-                'updated_at' => $date,
             ],
         ]);
     }
