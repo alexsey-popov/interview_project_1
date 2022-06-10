b60e8640a84a70deac1753c9cd0dd657da38f6c3
Alexsey <send.popov@mail.ru>
2022-06-10 13:14:34 +0300

    Feat. Add Equipment relation

--- app/Models/EquipmentType.php
+++ app/Models/EquipmentType.php
@@ -15,4 +15,13 @@ class EquipmentType extends Model
      * @var array
      */
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * Get equipment for this type
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function equipment()
+    {
+        return $this->hasMany(Equipment::class);
+    }
 }
