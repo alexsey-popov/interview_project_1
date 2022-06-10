ab4763bd87b3376d05ba79d32a000696e4b1611d
Alexsey <send.popov@mail.ru>
2022-06-10 13:14:21 +0300

    Feat. Add Equipment routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EquipmentTypeController;
+use \App\Http\Controllers\EquipmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('equipment-type', [EquipmentTypeController::class, 'index'])->name('equipment-type');
+
+Route::apiResource('equipment', EquipmentController::class);
