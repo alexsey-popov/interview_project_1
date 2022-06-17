6e2a93006173afbc62f41a78258e0930156e00f1
Alexsey <send.popov@mail.ru>
2022-06-17 13:28:36 +0300

    Feat. Add regex validation

--- app/Models/EquipmentType.php
+++ app/Models/EquipmentType.php
@@ -28,4 +28,55 @@ class EquipmentType extends Model
     {
         return $this->hasMany(Equipment::class);
     }
+
+    /**
+     * Get regexp by equipment type id
+     * @param $equipmentTypeId
+     * @return string
+     */
+    public static function getRegexpById($equipmentTypeId)
+    {
+        $equipmentType = self::find($equipmentTypeId);
+        if(empty($equipmentType)) {
+            return '';
+        }
+
+        return $equipmentType->convertMask();//'/[A-Z]/';
+    }
+
+    /**
+     * Convert mask to regexp
+     * @return string
+     */
+    public function convertMask()
+    {
+        $regexp = '/';
+        foreach (str_split($this->mask) as $char) {
+            $regexp .= $this->convertMaskChar($char);
+        }
+        $regexp .= '/';
+
+        return $regexp;
+    }
+
+    /**
+     * Convert mask char to regexp char
+     * @param $char
+     * @return string|void
+     */
+    public function convertMaskChar($char)
+    {
+        switch ($char) {
+            case 'N':
+                return '[0-9]';
+            case 'a':
+                return '[a-z]';
+            case 'A':
+                return '[A-Z]';
+            case 'X':
+                return '[A-Z0-9]';
+            case 'Z':
+                return '[-|_|@]';
+        }
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\EquipmentType;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Проверка серийного номера на соответсвие регулярному выражению его типа
+        Validator::extend('serial_number_mask', function ($attribute, $value, $parameters, $validator) {
+            $regexp = EquipmentType::getRegexpById($parameters[0]);
+            return Validator::make(
+                [ 'serial_number' => $value ],
+                [ 'serial_number' => 'regex:'.$regexp ],
+            )->passes();
+        });
     }
 }
