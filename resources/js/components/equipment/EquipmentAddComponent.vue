e46e2bbf9001b39d29a82c43f11bd5975b6ed23f
Alexsey <send.popov@mail.ru>
2022-06-17 15:39:56 +0300

    Refactor. Add equipments

--- resources/js/components/equipment/EquipmentAddComponent.vue
+++ resources/js/components/equipment/EquipmentAddComponent.vue
@@ -9,22 +9,21 @@
 
                     <div class="card-body">
 
-                        <form @submit.prevent="addEquipment">
+                        <form @submit.prevent="submit">
                         <fieldset :disabled="loading">
-                            <legend>Добавление оборудования</legend>
-                           
+                           <legend>Добавление оборудования</legend>
+
                            <div class="mb-3">
-                               <label for="equipmentType" class="form-label">Тип оборудования</label>
-                               <select v-model="equipment.equipmentType" class="form-select" id="equipmentType">
+                               <label for="equipment_type" class="form-label">Тип оборудования</label>
+                               <select v-model="equipment.equipment_type_id" class="form-select" id="equipment_type" required>
                                     <option disabled :value="null">Выберите тип оборудования</option>
-                                    <option v-for="equipmentType in equipmentTypes" :key="equipmentType.id" :value="equipmentType.id">{{ equipmentType.name }}</option>
-                                </select>
+                                    <option v-for="equipment_type in equipment_types" :key="equipment_type.id" :value="equipment_type.id">{{ equipment_type.name }}</option>
+                               </select>
                            </div>
-                            
 
                             <div class="mb-3">
-                               <label for="serialNumbers" class="form-label">Серийные номера</label>
-                               <textarea name="serialNumbers" id="serialNumbers" class="form-control" rows="10" v-model="equipment.serial_numbers" placeholder="Каждый серийный номер должен начинаться с новой строки."></textarea>
+                               <label for="serial_numbers" class="form-label">Серийные номера</label>
+                               <textarea v-model="serial_numbers" name="serial_numbers" id="serial_numbers" class="form-control" rows="10" placeholder="Перечислите серийные номера через запятую." required></textarea>
                             </div>
 
                             <div class="mb-3">
@@ -32,11 +31,13 @@
                                <textarea name="notes" id="notes" class="form-control" rows="3" v-model="equipment.notes" placeholder="Необязательное поле"></textarea>
                             </div>
 
-                             <button type="submit" class="btn btn-primary">Добавить</button>
+                            <div class="mt-3 text-center">
+                                <button type="submit" class="btn btn-primary">Добавить</button>
+                            </div>
 
                         </fieldset>
-                        </form>                      
-                       
+                        </form>
+
 
                         <error :haveEror="error"></error>
                         <spiner :loading="loading"></spiner>
@@ -55,11 +56,12 @@
         data() {
             return {
                 loading: true,
-                equipmentTypes: [],
+                equipment_types: [],
                 error: false,
+                serial_numbers: [],
 
                 equipment: {
-                    equipmentType: null,
+                    equipment_type_id: null,
                     serial_numbers: null,
                     notes: null,
                 }
@@ -68,7 +70,7 @@
         },
         mounted() {
             axios.get('/api/equipment-type/').then(response => {
-                this.equipmentTypes = response.data.data;
+                this.equipment_types = response.data.data;
             })
             .catch(error => {
                 this.error = true
@@ -77,8 +79,26 @@
                 this.loading = false
             })
         },
+        watch: {
+            serial_numbers: function () {
+                this.equipment.serial_numbers = this.serial_numbers.split(',')
+                    .map(element => element.trim())
+                    .filter(element => element !== '');
+            }
+        },
         methods: {
-            
+            submit() {
+                this.loading = true
+                axios.post('/api/equipment', this.equipment).then(response => {
+                    this.$router.push({ name:'equipment' })
+                })
+                    .catch(error => {
+                        this.error = true
+                    })
+                    .finally(() => {
+                        this.loading = false
+                    })
+            }
         },
     }
 </script>
