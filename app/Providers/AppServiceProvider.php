b6ce89b178acff9b95d031a1d71ab9ca4abedd97
Alexsey <send.popov@mail.ru>
2022-06-21 16:43:34 +0300

    Feat. Add auth chech validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 use App\Models\EquipmentType;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\ValidationException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,6 +35,15 @@ class AppServiceProvider extends ServiceProvider
                 [ 'serial_number' => $value ],
                 [ 'serial_number' => 'regex:'.$regexp ],
             )->passes();
+        });
+
+        // Проверка возможности авторизации
+        Validator::extend('auth_check', function ($attribute, $value, $parameters, $validator) {
+            $user = User::where('email', $value)->first();
+
+            $result = $user && Hash::check($parameters[0], $user->password);
+
+            return  $result;
         });
     }
 }
